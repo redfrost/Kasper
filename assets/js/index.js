597f062f2a10f7e6b88e04d85394006df526d24e
Alex <contact@simonalex.com>
2014-01-14 02:48:44 +1100

    Kasper for Ghost 0.3

---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+/**
+ * Main JS file for Casper behaviours
+ */
+
+/*globals jQuery, document */
+(function ($) {
+    "use strict";
+
+    $(document).ready(function(){
+
+        // On the home page, move the blog icon inside the header
+        // for better relative/absolute positioning.
+
+        //$("#blog-logo").prependTo("#site-head-content");
+
+    });
+
+}(jQuery));
+
+
+
+
+// Smooth Scroll To Top
+    $(document).ready(function() {
+		$('.back-top a').smoothScroll({offset: 0});
+	});
+
+
+// Parallax BG
+	jQuery(document).ready(function() {
+	  $('.no-touch body').stellar({
+	    horizontalScrolling: false,
+	    verticalOffset: -50
+	  });
+	});
+
+
+
+
+
+// Syntax Highlighting
+    $(document).ready(function() {
+        $( 'pre' ).addClass( 'prettyprint' );
+        $("pre[class=prettyprint]").each( function() {
+            if ( ($(this).html().split(/\n/).length-1) > 3 ) {
+                $(this).addClass( 'linenums' );
+            }
+        });
+    });
